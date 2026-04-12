1ca5e950333a916677f1e9674565db97ac818972
GG <test@gmail.con>
2026-04-12 11:59:54 +0200

    feat: link models to dog service functions

--- src/services/dog_service.py
+++ src/services/dog_service.py
@@ -1,11 +1,49 @@
 import random
-from src.api.schemas.dog import DogCreateRequest, Dog
+from datetime import datetime
 
+from src.api.schemas.dog import DogCreateRequest, DogRequest
+from src.db.models.dog import DogModel
 
-def create_dog(payload: DogCreateRequest) -> Dog:
-    return Dog(
-        id=random.randint(1, 1000),
-        name=payload.name,
-        breed=payload.breed,
-        age=payload.age,
+def list_dogs() -> list[DogModel]:
+    return [
+        DogModel(
+            dog_id="aaaa1111",
+            name="Milo",
+            device_id="aaaa1111",
+            start_lat=random.uniform(-90, 90),
+            start_lon=random.uniform(-180, 180),
+            is_active=True,
+            created_at=datetime.now(),
+        ),
+        DogModel(
+            dog_id="aaaa1111",
+            name="Milo",
+            device_id="aaaa1111",
+            start_lat=random.uniform(-90, 90),
+            start_lon=random.uniform(-180, 180),
+            is_active=True,
+            created_at=datetime.now(),
+        ),
+    ]
+
+def create_dog(payload: DogCreateRequest) -> DogModel:
+    return DogModel(
+        dog_id="aaaa1111",
+        name="Milo",
+        device_id="aaaa1111",
+        start_lat=random.uniform(-90, 90),
+        start_lon=random.uniform(-180, 180),
+        is_active=True,
+        created_at=datetime.now(),
+    )
+
+def get_dog(payload: DogRequest) -> DogModel:
+    return DogModel(
+        dog_id="aaaa1111",
+        name="Milo",
+        device_id="aaaa1111",
+        start_lat=random.uniform(-90, 90),
+        start_lon=random.uniform(-180, 180),
+        is_active=True,
+        created_at=datetime.now(),
     )
