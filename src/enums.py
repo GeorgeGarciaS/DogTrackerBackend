20f7a942570dfd28162df85062f53ff8dd24ca03
GG <test@gmail.con>
2026-04-16 01:07:52 +0200

    fix: issue with signal strength not valid, found with dbt tests

--- src/enums.py
+++ src/enums.py
@@ -24,6 +24,7 @@ class TelemetryIssueType(str, Enum):
     HEART_RATE_INVALID = "heart_rate_invalid"
     BATTERY_OUT_OF_RANGE = "battery_out_of_range"
     CUMULATIVE_STEPS_OUT_OF_RANGE = "cumulative_steps_out_of_range"
+    SIGNAL_STRENGTH = "signal_strength"
     DUPLICATE_EVENT = "duplicate_event"
     LATITUDE_OUT_OF_RANGE = LATITUDE_OUT_OF_RANGE
     LONGITUDE_OUT_OF_RANGE = LONGITUDE_OUT_OF_RANGE
